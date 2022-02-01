41079f083afe05eb71d0c999a3df19968b20acf5
Harry Prior <harry.prior@gfk.com>
2022-02-01 11:29:30 +0000

    Use inline record constructor

--- AutoDbPerf/Records/TableResult.cs
+++ AutoDbPerf/Records/TableResult.cs
@@ -1,16 +1,4 @@
 namespace AutoDbPerf.Records
 {
-    public record TableResult
-    {
-        public TableResult(float avgPlanningTime, float avgExecutionTime, string message = "")
-        {
-            AvgPlanningTime = avgPlanningTime;
-            AvgExecutionTime = avgExecutionTime;
-            Message = message;
-        }
-
-        public float AvgPlanningTime { get; }
-        public float AvgExecutionTime { get; }
-        public string Message { get; }
-    }
+    public record TableResult(float AvgPlanningTime, float AvgExecutionTime, string Message = "");
 }
